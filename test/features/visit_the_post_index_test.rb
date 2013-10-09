85b210ca06da5beaa9e173b36288205d33c344e3
Michael Frankenfield <mfrank183@gmail.com>
2013-10-09 15:44:23 -0700

    visit posts

---
+++ test/features/visit_the_post_index_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+feature "VisitThePostIndex" do
+  scenario "if posts exist, show all posts" do
+
+
+    #Given someone created a Post
+    Post.create(title: "Becoming a Code Fellow", body: "Means striving for excellence.")
+
+
+    #When someone visits the Posts Index
+    visit posts_path
+
+
+    #Then I should arrive on the Index Page
+    #  Then I should see the posts
+
+
+    page.text.must_include "Becoming a Code Fellow"
+
+  end
+end
