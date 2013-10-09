f2c4419476e73d26329a0b1376ca6c3ad9816250
Michael Frankenfield <mfrank183@gmail.com>
2013-10-09 16:22:42 -0700

    delete test

---
+++ test/features/delete_a_post_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+feature "DeleteAPost" do
+  scenario "deleting a post will remove it from Posts index page" do
+
+
+    #Given a user visits the Posts Index page
+    post = Post.create(title: "Becoming a Code Fellow", body: "Means striving for excellence.")
+
+    visit posts_path
+
+
+    #When user click the destroy post link
+
+    page.find("tr:last").click_on "Destroy"
+
+
+    #Then the index page will not have Post
+
+
+    page.text_wont_include "Becoming a Code Fellow"
+  end
+end
