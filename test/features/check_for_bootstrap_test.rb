8a8fb65185d35be9bf8a34b7727451b184bc27e4
Michael Frankenfield <mfrank183@gmail.com>
2013-10-10 22:01:41 -0700

    bootstrap 3.0

---
+++ test/features/check_for_bootstrap_test.rb
@@ -0,0 +1,22 @@
+# require "test_helper"
+
+# feature "CheckForBootstrap" do
+#   scenario "check for bootstrap loading on the Root Path" do
+
+
+#     #Given bootstrap is installed
+
+
+
+#     #When I visit the root path
+#     visit root_path
+
+
+#     #Then I have Bootstrap features
+
+
+#     page.body.must_include "col-"
+
+
+#   end
+# end
