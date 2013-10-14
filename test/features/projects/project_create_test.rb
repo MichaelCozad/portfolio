697b4a7e4fbb0bdcdd9600d8fafc0636a1c32a02
Michael Frankenfield <mfrank183@gmail.com>
2013-10-14 14:28:41 -0700

    Projects resource migration

---
+++ test/features/projects/project_create_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+feature "As a site owner, I want to add a portfolio item" do
+  scenario "create a project resource with valid data" do
+
+
+  #Given the user goes to create a new project
+  visit projects_path
+  click_on "New project"
+
+  fill_in "Name", with: "Faceblock"
+  fill_in "Technologies used", with: "Rails, Ruby, Bootstrap, HTML5, CSS3"
+
+  #When the user submits project information
+  click_on "Create project"
+
+
+
+  #Then the project information should show
+  page.text.must_have "Project was successfully added"
+  page.text.must_have "Faceblock"
+  page.text.must_have "Rails"
+
+  end
+end
+
