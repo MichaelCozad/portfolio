36416856415fe5a759bf89e22e1f182bc04d7771
Michael Frankenfield <mfrank183@gmail.com>
2013-10-15 23:11:48 -0700

    I can't find Sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_out_path_for(user)
+  root_path
+  end
+
+
 end
--- test/features/devise_test.rb
+++ test/features/devise_test.rb
@@ -1,19 +1,20 @@
 require 'test_helper'
 
+
 feature "I want to use Devise to restrict access to the site" do
   scenario "I want to sign up a new user account" do
 
-  #Given a registration form
-  visit "/"
-  click_on "Sign Up"
+  # Given a registration form
+  visit root_path
+  click_link "Sign up"
 
-  #When I register with valid data
+  # When I register with valid data
   fill_in "Email", with: "test@example.com"
   fill_in "Password", with: "password"
   fill_in "Password confirmation", with: "password"
-  click_on "Sign up"
+  click_button "Sign up"
 
-  #Then I should be signed up
+  # Then I should be signed up
   page.must_have_content "Welcome! You have signed up successfully"
   page.wont_have_content "There was a problem with your sign up"
   end
@@ -23,26 +24,33 @@ feature "I want to use Devise to restrict access to the site" do
 
   scenario "I want to sign out a user account" do
 
-  #Given
+  # Given a user is signed in.
+  visit root_path
+  click_link "Sign up"
+  fill_in "Email", with: users(:one).email
+  fill_in "Password", with: users(:one).encrypted_password
+  fill_in "Password confirmation", with: users(:one).encrypted_password
+  click_button "Sign up"
 
+  # When they click sign out
 
-  #When
+  click_on "Sign out"
 
-
-  #Then
+  # Then the session should be destroyed
+  page.text.must_include "Sign In"
 
   end
 
 
   scenario "I want to sign in with an existing user" do
 
-  #Given
+  #Given an existing user
 
 
-  #When
+  #When I click sign in
 
 
-  #Then
+  #Then I should see
 
 
   end
