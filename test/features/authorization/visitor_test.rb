0ce5c434e3c3bd460d827b43e187b9238c349ac4
Michael Frankenfield <mfrank183@gmail.com>
2013-10-19 14:36:29 -0700

    pundit tests

--- test/features/authorization/visitor_test.rb
+++ test/features/authorization/visitor_test.rb
@@ -9,13 +9,14 @@ scenario "Visitors should be able to read all posts" do
     fill_in "Password", with: "password"
     click_button "Sign in"
   #When
-
+  visit posts_path
+  page.find("tr:last")
   #Then
-
+  page.text.must_include("tr:last").title
   end
 
 
-  scenario "Visitors should not be able to delete, update or create posts" do
+  scenario "Visitors should not be able to delete a post" do
   #Given a user is signed in as an visitor
   visit new_user_session_path
     fill_in "Email", with: users(:three).email
@@ -24,6 +25,41 @@ scenario "Visitors should be able to read all posts" do
   #When
   visit posts_path
   page.find("tr:last").click_on "Destroy"
+  #Then
+  page.text.must_include 'not authorized'
+  end
+
+
+  scenario "Visitors should not be able to update a post" do
+  #Given a user is signed in as an visitor
+  visit new_user_session_path
+    fill_in "Email", with: users(:three).email
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+  #When they try to edit a post
+  visit post_path(posts(:peditor))
+  click_on("Edit")
+
+  fill_in 'Title', with: "Odoyle doesnt rule"
+
+  click_on "Update Post"
+
+  #Then
+  page.text.must_include 'not authorized'
+  end
+
+
+  scenario "Visitors should not be able to create a post" do
+  #Given a user is signed in as an visitor
+  visit new_user_session_path
+    fill_in "Email", with: users(:three).email
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+  #When they try to create a new post
+  visit new_post_path
+    fill_in 'Title', with: posts(:peditor).title
+    fill_in 'Body',  with: posts(:peditor).body
+    click_on 'Create Post'
   #Then
   page.text.must_include 'not authorized'
   end
