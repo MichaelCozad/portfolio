d6665fc6123095042dc873d3f06c0e305b8716f9
Michael Frankenfield <mfrank183@gmail.com>
2013-10-19 17:48:28 -0700

    view logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
 
   def after_sign_out_path_for(user)
   root_path
   end
 
+
+
+  private
+
+  def user_not_authorized
+    flash[:error] = "You are not authorized to perform this action."
+    redirect_to request.headers["Referer"] || root_path
+  end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ActiveRecord::Base
   belongs_to :author, class_name: "User"
 
-  attr_accessible :body, :title
+  attr_accessible :body, :title, :published
 
+
+  def publish!
+    published = true
+    save!
+  end
 
 end
