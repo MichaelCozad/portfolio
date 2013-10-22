5119e59f905e35c708711b2b3798207b60edffd2
Michael Frankenfield <mfrank183@gmail.com>
2013-10-22 14:57:35 -0700

    Omniauth migration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,7 +232,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-
+  config.omniauth :twitter, ENV["oUaeTw7Z29TnDcHMGMVi8w"], ENV["lxbd3eXn5a3gH1ovAlQFcfQJusEQUsA0XaFtnc4"]
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131021224812) do
+ActiveRecord::Schema.define(:version => 20131022215703) do
 
   create_table "comments", :force => true do |t|
     t.string   "author"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(:version => 20131021224812) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "role"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
