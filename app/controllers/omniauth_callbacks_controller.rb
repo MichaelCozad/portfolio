1ef74d0d21da0dc843f077d21f6d7ad6f91cc94a
Michael Frankenfield <mfrank183@gmail.com>
2013-10-22 15:35:58 -0700

    user name migration

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,2 +1,15 @@
-class OmniauthCallbacksController < ApplicationController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def all
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      flash.notice = "#{user.name}, you are sign in!"
+      sign_in_and_redirect user, notice: "#{user.name}, you are signed in!"
+    else
+      session["devise.user_attributes"] = user.user_attributes
+      redirect_to new_user_registration_url
+    end
+  end
+  alias_method :twitter, :all
+
 end
