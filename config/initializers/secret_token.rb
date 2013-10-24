c4ecce3baf72798a3a04fd2be5fa5d52d3fe2183
Michael Frankenfield <mfrank183@gmail.com>
2013-10-23 20:20:07 -0700

    fixed nuke self in foot

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Portfolio::Application.config.secret_token = '98495db3574307784077ed801ad5e91d4cefe3bd6956ad2550c5fc7507ccb9dca8c4213e3ed045f66bd94bea2f1ae37c3398b4b410164711763facd583ab9977'
+Portfolio::Application.config.secret_token = ENV['RAILS_SECRET']
