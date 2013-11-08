8d71fc78472de2f2e10053c884c1e46d1986ab7c
Michael Frankenfield <mfrank183@gmail.com>
2013-11-08 12:36:05 -0800

    Projects show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :name, :technologies_used, :published, :title, :body
+  attr_accessible :name, :technologies_used, :published, :title, :body, :img1, :img2, :img3, :img4, :img5, :img6, :img7, :img8
   has_many :comments, as: :commentable
 
   validates :technologies_used, presence: true
